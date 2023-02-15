fdb1e78240d5b58dec5f0de47ca217d9897a0ad7
Supreeti <sksupreetk11@gmail.com>
2023-02-15 18:16:27 +0530

    resolve linters

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const bar = document.querySelector('.menubar');
-const openbar = document.querySelector('.menuclick');
-const closebar = document.querySelector('.exit');
+const bar = document.querySelector(".menubar");
+const openbar = document.querySelector(".menuclick");
+const closebar = document.querySelector(".exit");
 
 function open() {
-    openbar.style.display = 'block';
-    openbar.classList.toggle('open');
-    bar.style.visibility = 'hidden';
-    closebar.style.visibility = 'visible';
-  }
-  
-  function close() {
-    openbar.style.display = 'none';
-    openbar.classList.remove('open');
-    bar.style.visibility = 'visible';
-    closebar.style.visibility = 'hidden';
-  }
-  
-  bar.addEventListener('click', open);
-  closebar.addEventListener('click', close);
-  openbar.addEventListener('click', close);
+  openbar.style.display = "block";
+  openbar.classList.toggle("open");
+  bar.style.visibility = "hidden";
+  closebar.style.visibility = "visible";
+}
+
+function close() {
+  openbar.style.display = "none";
+  openbar.classList.remove("open");
+  bar.style.visibility = "visible";
+  closebar.style.visibility = "hidden";
+}
+
+bar.addEventListener("click", open);
+closebar.addEventListener("click", close);
+openbar.addEventListener("click", close);
